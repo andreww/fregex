7bdbe16f5ac66f0d81dc19733c7c57594efd75a6
Andrew Walker <andreww@diopside.local>
2007-08-19 14:14:10 +0100

    Initial working version with support for:  + * ^ $ and literal characters.

---
+++ fexp.f90
@@ -0,0 +1,82 @@
+module fexp
+
+ implicit none
+
+contains
+
+ logical function match(regexp, text)
+
+    character(len=*), intent(in) :: regexp
+    character(len=*), intent(in) :: text
+
+    integer :: pos
+
+    if (regexp(1:1).eq."^") then
+        match = matchhere(regexp(2:len(regexp)), text)
+    else
+
+        do pos = 1, len(text)
+            if ( matchhere(regexp, text(pos:len(text))) ) then
+                match = .true.
+                exit
+            endif
+            match = .false.
+        enddo
+    endif
+
+  end function match
+
+  logical recursive function matchhere(regexp, text) result(res)
+
+    character(len=*), intent(in) :: regexp
+    character(len=*), intent(in) :: text
+
+    if (len(regexp).eq.1) then 
+        if (text(1:1).eq.regexp(1:1)) then
+                res = .true.
+        elseif (regexp(1:1).eq.".") then
+                res = .true.
+        elseif ((regexp(1:1).eq."$").and.(len(text).eq.0)) then
+                res = .true.
+        else 
+                res = .false.
+        endif
+    elseif (regexp(2:2).eq."*") then
+        res = matchstar(regexp(1:1), regexp(3:len(regexp)), text)
+    elseif (regexp(2:2).eq."+") then
+        res = matchhere(regexp(1:1)//regexp(1:1)//'*'//regexp(3:len(regexp)), text)
+    elseif (text(1:1).eq.regexp(1:1)) then
+        res =  matchhere( regexp(2:len(regexp)), text(2:len(text)) )
+    elseif (regexp(1:1).eq.".") then
+        res =  matchhere( regexp(2:len(regexp)), text(2:len(text)) )
+    else 
+        res = .false.
+    endif
+
+  end function matchhere
+
+  logical function matchstar(starchar, regexp, text) 
+
+      character(len=1), intent(in) :: starchar
+      character(len=*), intent(in) :: regexp
+      character(len=*), intent(in) :: text
+
+      integer :: pos
+      
+      pos = 1
+      do 
+          if (matchhere(regexp, text)) then
+              matchstar = .true.
+              exit
+          elseif ((pos.ne.len(text)).and.( (text(pos+1:pos+2).eq.starchar).or.(text(pos+1:pos+2).eq.".") )) then
+              pos = pos+1
+          else
+              matchstar = .false.
+              exit
+          endif
+      enddo
+
+
+  end function matchstar
+
+end module fexp
---
+++ test.f90
@@ -0,0 +1,33 @@
+program test
+
+use fexp
+
+print*, "Regexp, String, Result, expected result"
+print*, "Foo, FooBar, ", match("Foo", "FooBar"), "true"
+print*, "Poo, FooBar, ", match("Poo", "FooBar"), "false"
+print*, "Bar, FooBar, ", match("Bar", "FooBar"), "true"
+print*, "Par, FooBar, ", match("Par", "FooBar"), "false"
+print*, "Foo, Foo, ", match("Foo", "Foo"), "true"
+print*, "Fo, Foo, ", match("Fo", "Foo"), "true"
+print*, "Foo, Fo, ", match("Foo", "Fo"), "false"
+print*, "ooB, FooBar, ", match("ooB", "FooBar"), "true"
+print*, "ooP, FooBar, ", match("ooP", "FooBar"), "false"
+print*, "., FooBar, ", match(".", "FooBar"), "true"
+print*, "P., FooBar, ", match("P.", "FooBar"), "false"
+print*, "^Foo, FooBar, ", match("^Foo", "FooBar"), "true"
+print*, "^Bar, FooBar, ", match("^Bar", "FooBar"), "false"
+print*, "Foo$, FooBar, ", match("Foo$", "FooBar"), "false"
+print*, "Bar$, FooBar, ", match("Bar$", "FooBar"), "true"
+print*, ".*o, FooBar, ", match(".*o", "FooBar"), "true"
+print*, "o*o, FooBar, ", match("o*o", "FooBar"), "true"
+print*, "P*o, FooBar, ", match("P*o", "FooBar"), "true"
+print*, "Fo*o, FooBar, ", match("Fo*o", "FooBar"), "true"
+print*, "Po*o, FooBar, ", match("Po*o", "FooBar"), "false"
+print*, ".+o, FooBar, ", match(".+o", "FooBar"), "true"
+print*, "o+o, FooBar, ", match("o+o", "FooBar"), "true"
+print*, "P+o, FooBar, ", match("P+o", "FooBar"), "false"
+print*, "Fo+o, FooBar, ", match("Fo+o", "FooBar"), "true"
+print*, "Po+o, FooBar, ", match("Po+o", "FooBar"), "false"
+
+
+end program test
