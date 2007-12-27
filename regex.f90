fd72baf940f2c7560dc91a86dabac64e0aa75434
Andrew Walker <andreww@diopside.local>
2007-12-27 17:15:24 +0000

    New interface module.

---
+++ regex.f90
@@ -0,0 +1,34 @@
+module regex
+
+  implicit none
+
+  private
+
+  public :: new_re, match_re, sub_re
+
+contains
+
+  function new_re (regex_str)
+
+    character (len=*) :: regex_str
+    character (len=250) :: new_re
+    
+  end function new_re
+
+  function match_re (regex, str)
+
+    character (len=250) :: regex
+    character (len=*) :: str
+    logical :: match_re
+
+  end function match_re
+
+  function sub_re (regex, str)
+
+    character (len=250) :: regex
+    character (len=*) :: str
+    logical :: sub_re
+
+  end function sub_re
+
+end module regex
