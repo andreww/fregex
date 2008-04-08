69e15b770376269201ce0aa1f77e5e5387efe516
Andrew Walker <andreww@diopside.local>
2008-04-08 11:43:55 +0100

    New test harness. test.2 needs updating and there are four bugs exposed by test.1

---
+++ tests/testdrive.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $text;
+my $regex;
+my $match;
+my $start;
+my $length;
+
+my $line;
+my $text_line;
+my $regex_line;
+
+while (<>) {
+        $line ++;
+	if (/^Text: (.*)$/) {
+		$text = $1;
+                $text_line = $line;
+	} elsif (/^Regex: (.*)$/) {
+		$regex = $1;
+                $regex_line = $line;
+	} elsif ( (/^(Match: T 0s: \d+ 0l: \d+)\s+$/) or
+		  (/^(Match: F)\s+$/) ) {
+		my $expected = $1;
+		my $actual = &runtest($text, $regex);
+		if ($expected eq $actual) {
+			print "Ok\n";
+		} else {
+			print "Error: with text '$text' (line $text_line)  and regex ' $regex' (line $regex_line). I expected: '$expected' but got: '$actual'\n";
+		}
+	}
+}
+
+sub runtest {
+	my ($text, $regex) = @_;
+	my $result = `../regex_test '$regex' '$text'`;
+	chomp($result);
+	$result =~ s/^\s+(.*)/$1/;
+	return $result;
+}
